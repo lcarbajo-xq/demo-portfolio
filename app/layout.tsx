e4a13a173b854e4bfc1854e61798428505a4f2d5
lcarbajo-xq <luis.carbajo.arguelles@gmail.com>
2023-09-26 18:58:14 +0200

    [fix] og meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,14 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'Luis Carbajo Portfolio',
-  description: 'Luis Carbajo Projects'
+  description: 'Luis Carbajo Projects',
+  other: {
+    'theme-color': '#0d1117',
+    'color-scheme': 'dark only',
+    'og:url': 'https://demo-portfolio-3y6b.vercel.app/',
+    'og:image': '	https://demo-portfolio-3y6b.vercel.app/hero-background.avif',
+    'og:type': 'website',
+  }
 }
 
 export default function RootLayout({
